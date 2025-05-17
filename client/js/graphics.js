a045ebd5295cd74d4ba4d43a7ec57412954b90f3
nelimalu <lukaj2501@gmail.com>
2025-05-16 23:20:10 -0400

    created client boilerplate

---
+++ client/js/graphics.js
@@ -0,0 +1,17 @@
+
+
+function setup() {
+	console.log("SETUP");
+	let canvas = createCanvas(windowWidth, windowHeight);
+	canvas.position(0,0);
+
+	background(0,0,0);
+}
+
+
+function draw() {
+	console.log("test");
+
+	stroke(255,255,255);
+	line(30, 20, 85, 75);
+}
